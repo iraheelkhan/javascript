1084026641760a6f7372e373a172f53eb687bfd4
nsol hub <44431757+nsolhub@users.noreply.github.com>
2019-01-24 19:08:23 +0500

    Add files via upload
    changes has been made to the files. 
    class toggle
    get element by id
    ternary operator/condition

--- starter/app.js
+++ starter/app.js
@@ -14,38 +14,57 @@ var scores, roundScore, activePlayer, dice;
 
 scores = [0,0];
 roundScore = 0 ;
-activePlayer = 1;
-
-
-
-
+activePlayer = 0;
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 // hiding the dice on load of windows
 document.querySelector('.dice').style.display = 'none';
 
 //here are the two different ways to handle the event listener.
-
 // the first one is to make it happen is create a separate function for whatever you want to do by that event and pass the name of that function as a parameter to the addEventListener. which is done below. 
 
 /*
-
 function btnClicked(){
 	//code goes here for something making to happen. 
 }
 document.querySelector('.btn-roll').addEventListener('click', btnClicked);
-
 */
 //the other way around to initiate the function within the addeventlistener function like the below one. 
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
 	// your code might goes here for the function which is initiated and passed as parameter in addEventListener
 
-	//generating a random number within the limit of 0-5 + 1 (so it should get within the limit of 1+5) and displaying the generated number on the specific id.
+	//generating a random number within the limit of 0-5 + 1 (so it should get within the limit of 1-6) and displaying the generated number on the specific id.
 	dice = Math.floor(Math.random() * 6) + 1;
-	document.querySelector('#current-1').textContent = dice;
+
+	//checking if the dice is not equals 1 to.
+	if(dice !== 1){
+		roundScore += dice;
+		document.querySelector('#current-' + activePlayer).textContent = roundScore;
+		//activePlayer = 0;
+	} else{
+		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+
+		roundScore = 0;
+		
+		
+		document.getElementById('player-0').classList.toggle('active');
+		document.getElementById('player-1').classList.toggle('active');
+		// document.getElementById('player-0').classList.remove('active');
+		// document.getElementById('player-1').classList.add('active');
+		document.querySelector('.dice').style.display = 'none';
+
+
+	}
 
 	//showing the number dice image on click event.
 	imageDOM = document.querySelector('.dice');
 	imageDOM.style.display = 'block';
+	imageDOM.src = 'dice-'+ dice + '.png';
+
+
 
 	
 
