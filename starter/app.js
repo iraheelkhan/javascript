fb26504c859df7aa61db29f57e8570170ce09ee6
nsol hub <44431757+nsolhub@users.noreply.github.com>
2019-01-26 19:07:11 +0500

    Add files via upload
    game completed. 
    challenge not yet.

--- starter/app.js
+++ starter/app.js
@@ -12,16 +12,8 @@ GAME RULES:
 
 var scores, roundScore, activePlayer, dice;
 
-scores = [0,0];
-roundScore = 0 ;
-activePlayer = 0;
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-// hiding the dice on load of windows
-document.querySelector('.dice').style.display = 'none';
 
+init();
 //here are the two different ways to handle the event listener.
 // the first one is to make it happen is create a separate function for whatever you want to do by that event and pass the name of that function as a parameter to the addEventListener. which is done below. 
 
@@ -45,6 +37,41 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 		document.querySelector('#current-' + activePlayer).textContent = roundScore;
 		//activePlayer = 0;
 	} else{
+		document.querySelector('#current-' + activePlayer).textContent = 0;
+		nextPlayer();
+	}
+
+	//showing the number dice image on click event.
+	imageDOM = document.querySelector('.dice');
+	imageDOM.style.display = 'block';
+	imageDOM.src = 'dice-'+ dice + '.png';
+
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+
+	scores[activePlayer] += roundScore;
+
+	document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+	if(scores[activePlayer] >= 10){
+		document.querySelector('.dice').style.display = 'none';
+		document.getElementById('name-' + activePlayer).innerHTML = "WINNER";
+		document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+
+		//disabling all the buttons except the new game
+		document.querySelector('.btn-roll').disabled  = true;
+		document.querySelector('.btn-hold').disabled  = true;
+		
+	}else{
+		nextPlayer();
+
+	}
+});
+
+document.querySelector('.btn-new').addEventListener('click', init);
+function nextPlayer(){
 		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
 
 		roundScore = 0;
@@ -55,19 +82,28 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 		// document.getElementById('player-0').classList.remove('active');
 		// document.getElementById('player-1').classList.add('active');
 		document.querySelector('.dice').style.display = 'none';
+}
 
+function init(){
+	scores = [0,0];
+	roundScore = 0 ;
+	activePlayer = 0;
 
-	}
-
-	//showing the number dice image on click event.
-	imageDOM = document.querySelector('.dice');
-	imageDOM.style.display = 'block';
-	imageDOM.src = 'dice-'+ dice + '.png';
-
-
-
+	document.getElementById('score-0').textContent = '0';
+	document.getElementById('score-1').textContent = '0';
+	document.getElementById('current-0').textContent = '0';
+	document.getElementById('current-1').textContent = '0';
+	document.getElementById('name-0').textContent = 'Player 1';
+	document.getElementById('name-1').textContent = 'Player 2';
 	
+	document.querySelector('.player-' + activePlayer + '-panel').classList.remove('winner');
+	document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+	document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+	document.querySelector('.player-0-panel').classList.add('active');
+	// hiding the dice on load of windows
+	document.querySelector('.dice').style.display = 'none';
 
-
-});
+	document.querySelector('.btn-roll').disabled  = false;
+		document.querySelector('.btn-hold').disabled  = false;
+}
 
